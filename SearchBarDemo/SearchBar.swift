19418c6849ef104d5ff97069b69d52207618b0c7
Kristijan Kralj <kristijan.kralj@live.com>
2020-04-04 13:47:49 +0200

    Remove border around search bar

--- SearchBarDemo/SearchBar.swift
+++ SearchBarDemo/SearchBar.swift
@@ -23,6 +23,8 @@ struct SearchBar : UIViewRepresentable {
     -> UISearchBar {
       
       let searchBar = UISearchBar(frame: .zero)
+      searchBar.layer.borderWidth = 1
+      searchBar.layer.borderColor = UIColor.white.cgColor
       searchBar.delegate = context.coordinator
       return searchBar
   }
@@ -37,12 +39,22 @@ struct SearchBar : UIViewRepresentable {
     {
       _text = text
     }
-    func searchBar(_ searchBar: UISearchBar,
-                   textDidChange searchText: String)
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String)
     {
       text = searchText
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+      searchBar.endEditing(true)
     }
   }
 }
 
 
+
+struct SearchBar_Previews: PreviewProvider {
+  static var previews: some View {
+
+    SearchBar(text: .constant("Hello"))
+  }
+}
